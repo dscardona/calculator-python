ce653a331ce5d33fc4a7dc49cc5289d0be4a7c2e
dscardona <89112552+dscardona@users.noreply.github.com>
2023-05-12 14:17:58 +0000

    Initialize two variables with empty lists for hands of cards

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ def deal_card():
   """"outputs random card from list of cards"""
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
   return random.choice(cards)
+
+user_cards = []
+computer_cards = []
