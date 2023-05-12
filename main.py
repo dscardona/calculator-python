456cb40d672c12a884e79b3aac0101e6d6270527
dscardona <89112552+dscardona@users.noreply.github.com>
2023-05-12 15:17:56 +0000

    Create play_game logic, and implement while loop to continue game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from replit import clear
 
 def deal_card():
   """"Outputs random card from list of cards"""
@@ -34,34 +35,39 @@ def comprare_scores(user_score, computer_score):
   elif user_score < computer_score:
     return "You lose."
 
-user_cards = []
-computer_cards = []
-is_game_over = False
-
-for _ in range(2):
-  user_cards.append(deal_card())
-  computer_cards.append(deal_card())
+def play_game():
+  user_cards = []
+  computer_cards = []
+  is_game_over = False
   
-while not is_game_over:
-  user_score = check_score(user_cards)
-  computer_score = check_score(computer_cards)
+  for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+    
+  while not is_game_over:
+    user_score = check_score(user_cards)
+    computer_score = check_score(computer_cards)
+    
+    print(f"Your cards: {user_cards}, your score: {user_score}")
+    print(f"Computer's first card: {computer_cards[0]}")
   
-  print(f"Your cards: {user_cards}, your score: {user_score}")
-  print(f"Computer's first card: {computer_cards[0]}")
-
-  if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
-  else: 
-    deal_new = input("Do you want another card? Y/n\n")
-    if deal_new == "n":
+    if user_score == 0 or computer_score == 0 or user_score > 21:
       is_game_over = True
-    else:
-      user_cards.append(deal_card())
+    else: 
+      deal_new = input("Do you want another card? Y/n\n")
+      if deal_new == "n":
+        is_game_over = True
+      else:
+        user_cards.append(deal_card())
+  
+  while computer_score < 17 and computer_score != 0:
+    computer_cards.append(deal_card())
+    computer_score = check_score(computer_cards)
+  
+  print(comprare_scores(user_score, computer_score))
+  print(f" Your final hand: {user_cards}, your final score: {user_score}")
+  print(f" The house's hand: {computer_cards}, the house's score: {computer_score}")
 
-while computer_score < 17 and computer_score != 0:
-  computer_cards.append(deal_card())
-  computer_score = check_score(computer_cards)
-
-print(comprare_scores(user_score, computer_score))
-print(f" Your final hand: {user_cards}, your final score: {user_score}")
-print(f" The house's hand: {computer_cards}, the house's score: {computer_score}")
+while input("Do you want to play a game of Blackjack? y/n\n") == "y":
+  clear()
+  play_game()
