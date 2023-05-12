d32094a5569e50148af3ddef0f339739243df393
dscardona <89112552+dscardona@users.noreply.github.com>
2023-05-12 14:26:59 +0000

    Create check_score function

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-# Deal card function
 import random
 
 def deal_card():
-  """"outputs random card from list of cards"""
+  """"Outputs random card from list of cards"""
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
   return random.choice(cards)
+
+def check_score(cards):
+  """"Returns sum/score of given hand of cards"""
+  if sum(cards) == 21 and len(cards) == 2:
+    # 0 will represent a hand of Blackjack
+    return 0
+  if sum(cards) > 21 and 11 in cards:
+    # The number 11 represents an Ace, which can have value of 11 or 1
+    cards.remove(11)
+    cards.append(1)
+  return sum(cards)
 
 user_cards = []
 computer_cards = []
@@ -13,5 +23,3 @@ for _ in range(2):
   user_cards.append(deal_card())
   computer_cards.append(deal_card())
 
-print(user_cards)
-print(computer_cards)
