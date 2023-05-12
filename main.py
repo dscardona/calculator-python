bad05a9d8fe37c7c5ba3611713095345c6796cf0
dscardona <89112552+dscardona@users.noreply.github.com>
2023-05-12 14:20:05 +0000

    Assign two cards to each player with for/in range

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def deal_card():
 
 user_cards = []
 computer_cards = []
+
+for _ in range(2):
+  user_cards.append(deal_card())
+  computer_cards.append(deal_card())
+
+print(user_cards)
+print(computer_cards)
